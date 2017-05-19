fb1b5f8978b477cc8ca61e6360c096704b28bc86
theaayushsharma <theaayushsharma@outlook.com>
2017-05-19 11:52:44 +0530

    Add image for main activity

--- app/src/main/java/com/android/admob/MainActivity.java
+++ app/src/main/java/com/android/admob/MainActivity.java
@@ -1,22 +1,11 @@
 package com.android.admob;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.SpannableString;
-import android.text.style.UnderlineSpan;
-import android.util.Log;
-import android.widget.Toast;
-
-import com.getkeepsafe.taptargetview.TapTarget;
-import com.getkeepsafe.taptargetview.TapTargetView;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String PREF_FIRSTLAUNCH_HELP = "helpDisplayed";
-    private boolean helpDisplayed = false;
 
     private AdView adView;
 
@@ -29,8 +18,6 @@ public class MainActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder()
                 .build();
         adView.loadAd(adRequest);
-
-        showHelpForFirstLaunch();
     }
 
     @Override
@@ -55,60 +42,5 @@ public class MainActivity extends AppCompatActivity {
             adView.destroy();
         }
         super.onDestroy();
-    }
-
-    private void showHelpForFirstLaunch() {
-        if (helpDisplayed)
-            return;
-        helpDisplayed = getPreferenceValue(PREF_FIRSTLAUNCH_HELP, false);
-        if (!helpDisplayed) {
-            savePreference(PREF_FIRSTLAUNCH_HELP, true);
-            showHelp();
-        }
-    }
-
-    private boolean getPreferenceValue(String key, boolean defaultValue) {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        return preferences.getBoolean(key, defaultValue);
-    }
-
-    private void savePreference(String key, boolean value) {
-        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean(key, value);
-        editor.commit();
-    }
-
-    private void showHelp() {
-
-        final SpannableString spannedDesc = new SpannableString("You can click on this ad");
-        spannedDesc.setSpan(new UnderlineSpan(), spannedDesc.length() - "click".length(), spannedDesc.length(), 0);
-        TapTargetView.showFor(this, TapTarget.forView(findViewById(R.id.adView), "This is Google AdMob", spannedDesc)
-                .dimColor(android.R.color.black)
-                .outerCircleColor(R.color.accent)
-                .targetCircleColor(android.R.color.black)
-                .transparentTarget(true)
-                .textColor(android.R.color.black)
-                .drawShadow(true)
-                .titleTextDimen(R.dimen.title_text_size)
-                .tintTarget(false), new TapTargetView.Listener() {
-            @Override
-            public void onTargetClick(TapTargetView view) {
-                super.onTargetClick(view);
-                // .. which evidently starts the sequence we defined earlier
-//                sequence.start();
-            }
-
-            @Override
-            public void onOuterCircleClick(TapTargetView view) {
-                super.onOuterCircleClick(view);
-                Toast.makeText(view.getContext(), "Please tap on icon to dismiss tutorial", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onTargetDismissed(TapTargetView view, boolean userInitiated) {
-                Log.d("TapTargetViewSample", "Continue");
-            }
-        });
     }
 }
